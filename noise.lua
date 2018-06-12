8f38bb34c263c2de297ce0b6032e347e32b4bb34
Jacob Carroll <jac21934@vt.edu>
2018-06-12 11:18:34 -0600

    Create noise.lua

---
+++ noise.lua
@@ -0,0 +1,351 @@
+-- Path to OpenPV. Replace this with an absolute path.
+package.path = package.path .. ";" .. "/home/jcarroll/OpenPV/parameterWrapper/?.lua";
+local pv = require "PVModule";
+
+if ((arg[1] == nil) or (arg[2] == nil)) then
+	 print("ERROR\n")
+	 print("Usage: lua " .. arg[0] .. " <VTHRESH>  <NUMFEATURES>");
+	 return -1;
+end
+
+
+local VThresh          = tonumber(arg[1]);  -- .005; --The threshold, or lambda, of the network
+local dictionarySize   = tonumber(arg[2]);   --Number of patches/elements in dictionary 
+
+-- Variables:
+local nbatch           = 20;    --Number of images to process in parallel
+local nxSize           = 32;    --CIFAR images are 32 x 32
+local nySize           = 32;
+local patchSize        = 8;
+local stride           = 2
+local displayPeriod    = 250;   --Number of timesteps to find sparse approximation
+local numEpochs        = 1;     --Number of times to run through dataset
+local numImages        = 10000; --Total number of images in dataset
+local stopTime         = math.ceil((numImages  * numEpochs) / nbatch) * displayPeriod;
+local writeStep        = displayPeriod; 
+local initialWriteTime = displayPeriod; 
+
+local inputPath        = "/home/jcarroll/CIFAR/cifar-10-batches-mat/test_batch_randorder.txt";
+local outputPath       = "./output/";
+local checkpointPeriod = (displayPeriod * 100); -- How often to write checkpoints
+
+local plasticityFlag   = false;  --Determines if we are learning our dictionary or holding it constant
+local momentumTau      = 500;   --Weight momentum parameter. A single weight update will last for momentumTau timesteps.
+local dWMax            = 0.05;    --The learning rate
+local AMin             = 0;
+local AMax             = infinity;
+local AShift           = VThresh;  --This being equal to VThresh is a soft threshold
+local VWidth           = 0; 
+local timeConstantTau  = 100;   --The integration tau for sparse approximation
+local weightInit       = 1.0;
+
+local dictionaryFile   = "/home/jcarroll/Trained_Weights/CIFAR/nf_" .. dictionarySize .. "/VThresh_" .. VThresh .. "/V1ToInputError_W.pvp";   --nil for initial weights, otherwise, specifies the weights file to load.
+
+
+-- Base table variable to store
+local pvParameters = {
+
+   --Layers------------------------------------------------------------
+   --------------------------------------------------------------------   
+   column = {
+      groupType = "HyPerCol";
+      startTime                           = 0;
+      dt                                  = 1;
+      stopTime                            = stopTime;
+      progressInterval                    = (displayPeriod * 10);
+      writeProgressToErr                  = true;
+      verifyWrites                        = false;
+      outputPath                          = outputPath;
+      printParamsFilename                 = "CIFAR_Tutorial.params";
+      randomSeed                          = 1234567890;
+      nx                                  = nxSize;
+      ny                                  = nySize;
+      nbatch                              = nbatch;
+      checkpointWrite                     = true;
+      checkpointWriteDir                  = outputPath .. "/Checkpoints"; --The checkpoint output directory
+      checkpointWriteTriggerMode          = "step";
+      checkpointWriteStepInterval         = checkpointPeriod; --How often to checkpoint
+      checkpointIndexWidth                = -1; -- Automatically select width of index in checkpoint directory name
+      deleteOlderCheckpoints              = false;
+      suppressNonplasticCheckpoints       = false;
+      initializeFromCheckpointDir         = "";
+      errorOnNotANumber                   = false;
+   };
+
+   AdaptiveTimeScales = {
+      groupType = "AdaptiveTimeScaleProbe";
+      targetName                          = "V1EnergyProbe";
+      message                             = nil;
+      textOutputFlag                      = true;
+      probeOutputFile                     = "AdaptiveTimeScales.txt";
+      triggerLayerName                    = "Input";
+      triggerOffset                       = 0;
+      baseMax                             = 0.06;  -- Initial upper bound for timescale growth
+      baseMin                             = 0.05;  -- Initial value for timescale growth
+      tauFactor                           = 0.03;  -- Percent of tau used as growth target
+      growthFactor                        = 0.025; -- Exponential growth factor. The smaller value between this and the above is chosen. 
+      writeTimeScalesFieldnames           = false;
+   };
+
+   Input = {
+      groupType = "ImageLayer";
+      nxScale                             = 1;
+      nyScale                             = 1;
+      nf                                  = 3;
+      phase                               = 0;
+      mirrorBCflag                        = true;
+      writeStep                           = writeStep;
+      initialWriteTime                    = initialWriteTime;
+      sparseLayer                         = false;
+      updateGpu                           = false;
+      dataType                            = nil;
+      inputPath                           = inputPath;
+      offsetAnchor                        = "tl";
+      offsetX                             = 0;
+      offsetY                             = 0;
+      inverseFlag                         = false;
+      normalizeLuminanceFlag              = true;
+      normalizeStdDev                     = true;
+      useInputBCflag                      = false;
+      autoResizeFlag                      = false;
+      displayPeriod                       = displayPeriod;
+      batchMethod                         = "byImage";
+      writeFrameToTimestamp               = true;
+      resetToStartOnLoop                  = false;
+   };
+
+   Noise = {
+      groupType = "NoiseLayer";
+      nxScale                             = 1;
+      nyScale                             = 1;
+      nf                                  = 3;
+      phase                               = 1;
+      mirrorBCflag                        = false;
+      valueBC                             = 0;
+      initializeFromCheckpointFlag        = false;
+      InitVType                           = "ZeroV";
+      triggerLayerName                    = "Input";
+      writeStep                           = writeStep;
+      initialWriteTime                    = initialWriteTime;
+      sparseLayer                         = false;
+      updateGpu                           = false;
+      dataType                            = nil;
+   };
+
+
+   InputError = {
+      groupType = "HyPerLayer";
+      nxScale                             = 1;
+      nyScale                             = 1;
+      nf                                  = 3;
+      phase                               = 1;
+      mirrorBCflag                        = false;
+      valueBC                             = 0;
+      initializeFromCheckpointFlag        = false;
+      InitVType                           = "ZeroV";
+      triggerLayerName                    = NULL;
+      writeStep                           = writeStep;
+      initialWriteTime                    = initialWriteTime;
+      sparseLayer                         = false;
+      updateGpu                           = false;
+      dataType                            = nil;
+   };
+
+   V1 = {
+      groupType = "HyPerLCALayer";
+      nxScale                             = 1/stride;
+      nyScale                             = 1/stride;
+      nf                                  = dictionarySize;
+      phase                               = 2;
+      mirrorBCflag                        = false;
+      valueBC                             = 0;
+      initializeFromCheckpointFlag        = false;
+      InitVType                           = "ConstantV";
+      valueV                              = VThresh;
+      triggerLayerName                    = NULL;
+      writeStep                           = writeStep;
+      initialWriteTime                    = initialWriteTime;
+      sparseLayer                         = true;
+      updateGpu                           = true;
+      dataType                            = nil;
+      VThresh                             = VThresh;
+      AMin                                = AMin;
+      AMax                                = AMax;
+      AShift                              = AShift;
+      VWidth                              = VWidth;
+      timeConstantTau                     = timeConstantTau;
+      selfInteract                        = true;
+      adaptiveTimeScaleProbe              = "AdaptiveTimeScales";
+   };
+
+   InputRecon = {
+      groupType = "HyPerLayer";
+      nxScale                             = 1;
+      nyScale                             = 1;
+      nf                                  = 3;
+      phase                               = 3;
+      mirrorBCflag                        = false;
+      valueBC                             = 0;
+      initializeFromCheckpointFlag        = false;
+      InitVType                           = "ZeroV";
+      triggerLayerName                    = NULL;
+      writeStep                           = writeStep;
+      initialWriteTime                    = initialWriteTime;
+      sparseLayer                         = false;
+      updateGpu                           = false;
+      dataType                            = nil;
+   };
+
+	 --Connections ------------------------------------------------------
+	 --------------------------------------------------------------------
+
+   InputToError = {
+      groupType = "RescaleConn";
+      preLayerName                        = "Input";
+      postLayerName                       = "InputError";
+      channelCode                         = 0;
+      delay                               = {0.000000};
+      scale                               = weightInit;
+   };
+
+	 NoiseToError = {
+      groupType = "IdentConn";
+      preLayerName                        = "Noise";
+      postLayerName                       = "InputError";
+      channelCode                         = 0;
+      delay                               = {0.000000};
+      initWeightsFile                     = nil;
+   };
+
+
+
+   ErrorToV1 = {
+      groupType = "TransposeConn";
+      preLayerName                        = "InputError";
+      postLayerName                       = "V1";
+      channelCode                         = 0;
+      delay                               = {0.000000};
+      convertRateToSpikeCount             = false;
+      receiveGpu                          = true;
+      updateGSynFromPostPerspective       = true;
+      pvpatchAccumulateType               = "convolve";
+      writeStep                           = -1;
+      writeCompressedCheckpoints          = false;
+      gpuGroupIdx                         = -1;
+      originalConnName                    = "V1ToInputError";
+   };
+
+   V1ToInputError = {
+      groupType = "MomentumConn";
+      preLayerName                        = "V1";
+      postLayerName                       = "InputError";
+      channelCode                         = -1;
+      delay                               = {0.000000};
+      numAxonalArbors                     = 1;
+      plasticityFlag                      = plasticityFlag;
+      convertRateToSpikeCount             = false;
+      receiveGpu                          = false; -- non-sparse -> non-sparse
+      sharedWeights                       = true;
+      weightInitType                      = "FileWeight";
+			initWeightsFile 										= dictionaryFile;
+      wMinInit                            = -1;
+      wMaxInit                            = 1;
+      sparseFraction                      = 0.9;
+      minNNZ                              = 0;
+      combineWeightFiles                  = false;
+      initializeFromCheckpointFlag        = false;
+      triggerLayerName                    = "Input";
+      triggerOffset                       = 0;
+      immediateWeightUpdate               = true;
+      updateGSynFromPostPerspective       = false; -- Should be false from V1 (sparse layer) to Error (not sparse). Otherwise every input from pre will be calculated (Instead of only active ones)
+      pvpatchAccumulateType               = "convolve";
+      writeStep                           = writeStep;
+      initialWriteTime                    = initialWriteTime;
+      writeCompressedWeights              = false;
+      writeCompressedCheckpoints          = false;
+      combine_dW_with_W_flag              = false;
+      nxp                                 = patchSize;
+      nyp                                 = patchSize;
+      normalizeMethod                     = "normalizeL2";
+      strength                            = 1;
+      normalizeArborsIndividually         = false;
+      normalizeOnInitialize               = true;
+      normalizeOnWeightUpdate             = true;
+      rMinX                               = 0;
+      rMinY                               = 0;
+      nonnegativeConstraintFlag           = false;
+      normalize_cutoff                    = 0;
+      normalizeFromPostPerspective        = false;
+      minL2NormTolerated                  = 0;
+      dWMax                               = dWMax; 
+      momentumTau                         = momentumTau;   --The momentum parameter. A single weight update will last for momentumTau timesteps.
+      momentumMethod                      = "viscosity";
+      momentumDecay                       = 0;
+   }; 
+
+   V1ToRecon = {
+      groupType = "CloneConn";
+      preLayerName                        = "V1";
+      postLayerName                       = "InputRecon";
+      channelCode                         = 0;
+      delay                               = {0.000000};
+      convertRateToSpikeCount             = false;
+      receiveGpu                          = false;
+      updateGSynFromPostPerspective       = false;
+      pvpatchAccumulateType               = "convolve";
+      writeCompressedCheckpoints          = false;
+      originalConnName                    = "V1ToInputError";
+   };
+
+   ReconToError = {
+      groupType = "IdentConn";
+      preLayerName                        = "InputRecon";
+      postLayerName                       = "InputError";
+      channelCode                         = 1;
+      delay                               = {0.000000};
+      initWeightsFile                     = nil;
+   };
+
+   --Probes------------------------------------------------------------
+   --------------------------------------------------------------------
+
+   V1EnergyProbe = {
+      groupType = "ColumnEnergyProbe";
+      message                             = nil;
+      textOutputFlag                      = true;
+      probeOutputFile                     = "V1EnergyProbe.txt";
+      triggerLayerName                    = nil;
+      energyProbe                         = nil;
+   };
+
+   InputErrorL2NormEnergyProbe = {
+      groupType = "L2NormProbe";
+      targetLayer                         = "InputError";
+      message                             = nil;
+      textOutputFlag                      = true;
+      probeOutputFile                     = "InputErrorL2NormEnergyProbe.txt";
+      energyProbe                         = "V1EnergyProbe";
+      coefficient                         = 0.5;
+      maskLayerName                       = nil;
+      exponent                            = 2;
+   };
+
+   V1L1NormEnergyProbe = {
+      groupType = "L1NormProbe";
+      targetLayer                         = "V1";
+      message                             = nil;
+      textOutputFlag                      = true;
+      probeOutputFile                     = "V1L1NormEnergyProbe.txt";
+      energyProbe                         = "V1EnergyProbe";
+      coefficient                         = VThresh;
+      maskLayerName                       = nil;
+   };
+
+} --End of pvParameters
+
+if dictionaryFile ~= nil then
+   pvParameters.V1ToInputError.weightInitType  = "FileWeight";
+   pvParameters.V1ToInputError.initWeightsFile = dictionaryFile;
+end
+-- Print out PetaVision approved parameter file to the console
+pv.printConsole(pvParameters)
