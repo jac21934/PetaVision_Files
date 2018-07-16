f7d2eb75ab1084e1646e2d5d45629f232104e551
Jacob Alexander Carroll - 322520 <carroll@darwin-fe2.lanl.gov>
2018-07-16 13:28:52 -0600

    new data

--- vanhateren/generate_list.sh
+++ vanhateren/generate_list.sh
@@ -9,5 +9,5 @@ done
 
 sort -R ordered_list > shuffled_list
 head -n $NUM_TRAIN shuffled_list > training_list
-tail -n $(( $NUM_TRAIN + 1 )) shuffled_list > testing_list
+tail -n +$(( $NUM_TRAIN + 1 )) shuffled_list > testing_list
 
