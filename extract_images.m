fc256c4bd3a9bd5119e21ed1a72878c5ffe53794
Jacob <jac21934@vt.edu>
2018-07-17 13:18:05 -0600

    visualizeFeatures.py

--- extract_images.m
+++ extract_images.m
@@ -1,40 +1,22 @@
 pkg load image
-addpath('/projects/pcsri/PetaVision/OpenPV/mlab/util/')
+addpath('/home/jacob/Code/OpenPV/mlab/util/')
 
-arg_list = argv ();
-if nargin != 3
-    printf("Usage: %s <output dir> <image name> <num images>\n", program_name());
-    exit(0);
-endif
+		arg_list = argv ();
+ 
 
-outputDir = arg_list{1};
-imgDir   = arg_list{2};
-numImages = str2num(arg_list{3});
-
-%%Input        = readpvpfile([outputDir '/Input.pvp']);
-%%NoiseLayer   = readpvpfile([outputDir '/NoiseLayer.pvp']);
-InputRecon   = readpvpfile([outputDir '/InputRecon_V.pvp']);
+InputRecon   = readpvpfile('./InputRecon_V.pvp');
 
 function img = getImg(file, index)
-    img = permute(file{index}.values, [2, 1, 3]);
-    minVal = min(img(:));
-    maxVal = max(img(:));
-    img = img .- minVal;
-    if maxVal - minVal > 1
-        img = img ./ (maxVal - minVal);
-    endif
+		img = permute(file{index}.values, [2, 1, 3]);
+		minVal = min(img(:));
+		maxVal = max(img(:));
+		img = img .- minVal;
+		if maxVal - minVal > 1
+			img = img ./ (maxVal - minVal);
+endif
 end
 
-for i = [1:numImages]
-    %%    InputImg = getImg(Input, i);
-    %%imwrite(InputImg, [imgDir "/" num2str(i) "/Input.png"]);
-
-    %%    NoiseLayerImg = getImg(NoiseLayer, i);
-    %%imwrite(NoiseLayerImg, [imgDir "/" num2str(i) "/NoiseLayer.png"]);
-     
-    InputReconImg = getImg(InputRecon, i);
-    imwrite(InputReconImg, [imgDir "/" num2str(i) "/InputRecon.png"]);
-
-    %%DenoiseErrorImg = abs(InputImg - InputReconImg);
-    %%imwrite(DenoiseErrorImg, [imgDir "/" num2str(i) "/DenoiseError.png"]);
+for i = [1:100]
+	InputReconImg = getImg(InputRecon, i);
+	imwrite(InputReconImg, ["./figures/InputRecon_" num2str(i) ".png"]);
 endfor
--- visualizeFeatures.py
+++ visualizeFeatures.py
@@ -1,29 +1,31 @@
 # Imports
 import os, sys
-lib_path = os.path.abspath("/projects/pcsri/PetaVision/OpenPV/python/")
+lib_path = os.path.abspath("/home/jacob/Code/OpenPV/python/")
 sys.path.append(lib_path)
 import pvtools as pv
 import numpy as np
 from matplotlib import pyplot as plt
 
-if len(sys.argv) != 2:
-    print 'Usage: ' + sys.argv[0] + ' <checkpoint directory>'
-    sys.exit()
 
-width  = 16
-height = 8
+width  = 1
+height = 1
 
 # Load and scale data
-dictionary = pv.readpvpfile(sys.argv[1] +  "/V1ToInputError_W.pvp")['values'][0,0,]
+dictionary = pv.readpvpfile("./V1ToInputError_W.pvp")['values'][0,0,]
 dictionary -= np.min(dictionary)
 dictionary /= np.max(dictionary)
 
-# Display
-fig, ax = plt.subplots(width,height,figsize=(3,3))
-for i in range(width):
-    for j in range(height):
-       ax[i,j].imshow(dictionary[height*i+j],vmin=0.0,vmax=1.0)
-       ax[i,j].set_axis_off()
-plt.show()
-#plt.savefig("feature_kernel.png", transparent=True)
+
+print"Shape:", dictionary[0].shape
+print"Values min/max:", dictionary.min(), dictionary.max()
+
+for i in range(0,128):
+    feature = dictionary[i]
+    
+    feature = feature.reshape(64,64)
+    
+    
+    plt.imshow(feature,cmap='gray')
+    plt.savefig("./figures/feature_" + str(i) + ".png") 
+
 
