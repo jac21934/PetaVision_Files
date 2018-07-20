d485f8e3c03f24b9d7b0ae9240333999bc947f61
Jacob <jac21934@vt.edu>
2018-07-20 14:29:50 -0600

    new file

--- noise_image.m
+++ noise_image.m
@@ -1,7 +1,8 @@
-addpath('/home/jacob/PetaVision_Data/PetaVision_Files/util')
+addpath('/home/jcarroll/OpenPV/mlab/util/')
 pkg load image
 
 
+#BaseString = '/home/jcarroll/runs/CIFAR_noise/nf128/VThresh_0.01/output/';
 
 arg_list = argv();
 
@@ -10,22 +11,52 @@ if(isempty(arg_list))
 	return;
 endif
 
-FileName=arg_list{1};
+BaseString=arg_list{1};
 
+InputString = [ BaseString "Input.pvp"];
+ReconString = [ BaseString "InputRecon.pvp"];
+NoiseString = [ BaseString "Noise.pvp"];
+ErrorString = [ BaseString "InputError.pvp"];
 		
-[noiseData, noiseHdr] = readpvpfile(FileName);
+[inputData, inputHdr] = readpvpfile(InputString);
+[reconData, reconHdr] = readpvpfile(ReconString);
+[noiseData, noiseHdr] = readpvpfile(NoiseString);
+[errorData, errorHdr] = readpvpfile(ErrorString);
 
-pictureIndex=1;
+pictureIndex=100;
 
+# Original Picture
+InputMax = max(inputData{pictureIndex}.values(:));
+InputMin = min(inputData{pictureIndex}.values(:));
+InputImage = (inputData{pictureIndex}.values .- InputMin);
+InputImage = InputImage./max(InputImage(:));
+InputImage = (permute(InputImage, [2,1,3]));
 # Noisy Picture
 NoiseMax = max(noiseData{pictureIndex}.values(:));
 NoiseMin = min(noiseData{pictureIndex}.values(:));
 NoiseImage = (noiseData{pictureIndex}.values .- NoiseMin);
 NoiseImage = NoiseImage./max(NoiseImage(:));
 NoiseImage = (permute(NoiseImage, [2,1,3]));
+# Input Error Picture
+ErrorMax = max(errorData{pictureIndex}.values(:));
+ErrorMin = min(errorData{pictureIndex}.values(:));
+ErrorImage = (errorData{pictureIndex}.values .- ErrorMin);
+ErrorImage = ErrorImage./max(ErrorImage(:));
+ErrorImage = (permute(ErrorImage, [2,1,3]));
 
-concatenated = imresize(horzcat(NoiseImage), 1);
+
+# Recostruction
+ReconMax = max(reconData{pictureIndex}.values(:));
+ReconMin = min(reconData{pictureIndex}.values(:));
+ReconImage = (reconData{pictureIndex}.values .- ReconMin);
+ReconImage = ReconImage./max(ReconImage(:));
+ReconImage = (permute(ReconImage, [2,1,3]));
+
+concatenated = imresize(horzcat(InputImage,NoiseImage,ErrorImage, ReconImage), 4);
 
 figure;
-imwrite(concatenated, "test2.png")
+imwrite(concatenated, "test.png")
 
+
+
+							 
